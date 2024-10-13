df4fdb576cacaba3199dd4425e597b6e69d14fd2
Maksim <112960156+TwinkleToesDev@users.noreply.github.com>
2024-10-13 17:13:33 +0400

    router fix

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return view('welcome'); // Замените 'welcome' на ваше основное представление, если оно отличается
+})->where('any', '.*');
